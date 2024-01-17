66baa4ad15379d03fc6d7ce5df68821ffd8efc9e
Rifat Jayed <rifat.jayed2000@gmail.com>
2024-01-17 23:34:40 +0600

    components folder create

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node:true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -13,7 +13,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
-      'warn',
+      'warn/off',
       { allowConstantExport: true },
     ],
   },
---
+++ src/Components/Meal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Meal = () => {
+    return (
+        <div>
+            <h1>Meal</h1>
+        </div>
+    );
+};
+
+export default Meal;
