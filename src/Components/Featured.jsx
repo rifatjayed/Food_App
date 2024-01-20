b387008e7a11493ef15217040ba1b44584682c16
Rifat Jayed <rifat.jayed2000@gmail.com>
2024-01-20 23:41:54 +0600

    slider work

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { BsChevronLeft, BsChevronRight  } from "react-icons/bs";
+
 
 const Featured = () => {
+
+    const sliders= [
+        {
+          url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672076/NetflixApp/burger_emxbtv.jpg'
+        },
+        {
+          url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672452/NetflixApp/pizza_osjb4f.jpg'
+        },
+        {
+          url: 'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672672612/NetflixApp/ric_a4ewxo.jpg',
+        },
+    
+    ]
+
+    const [currentIndex, setcurrentIndex]= useState(0)
     return (
-        <div>
-            <h1>Featured</h1>
+        <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative'>
+         <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500' style={{backgroundImage:`url(${sliders[currentIndex].url})`}}></div>
+<div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
+    <BsChevronLeft></BsChevronLeft>
+</div>
+
         </div>
     );
 };
