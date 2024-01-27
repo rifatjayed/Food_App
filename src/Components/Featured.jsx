8d5af7d7002d5662424848c092581abdf02ebec8
Rifat Jayed <rifat.jayed2000@gmail.com>
2024-01-28 00:43:08 +0600

    responsive without hero section

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -39,7 +39,7 @@ const Featured = () => {
 
     return (
         <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative group'>
-            <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500 object-cover' style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}></div>
+            <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500 ' style={{ backgroundImage: `url(${sliders[currentIndex].url})` }}></div>
             <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-orange-700 text-white cursor-pointer'>
                 <BsChevronLeft onClick={prevSlider}></BsChevronLeft>
             </div>
